d9b186ddf9af021baadc3a42b561ef5d0fac69b4
MikhailovaTatiana <Titanius8>
2020-05-07 16:09:50 +0200

    jquery correction

--- iceland/jquery.js
+++ iceland/jquery.js
@@ -2,50 +2,28 @@ $(document).ready(function(){
   $("#flip1").click(function(){
     $("#panel1").slideToggle("slow");
   });
+  $("#flip2").click(function(){
+    $("#panel2").slideToggle("slow");
+  });
+  $("#flip3").click(function(){
+    $("#panel3").slideToggle("slow");
+  });
+  $("#flip4").click(function(){
+    $("#panel4").slideToggle("slow");
+  });
+  $("#flip5").click(function(){
+    $("#panel5").slideToggle("slow");
+  });
+  $("#flip6").click(function(){
+    $("#panel6").slideToggle("slow");
+  });
+  $("#flip7").click(function(){
+    $("#panel7").slideToggle("slow");
+  });
+  $("#flip8").click(function(){
+    $("#panel8").slideToggle("slow");
+  });
 });
-
-$(document).ready(function(){
-    $("#flip2").click(function(){
-      $("#panel2").slideToggle("slow");
-    });
-  });
-
-  $(document).ready(function(){
-    $("#flip3").click(function(){
-      $("#panel3").slideToggle("slow");
-    });
-  });
-
-  $(document).ready(function(){
-    $("#flip4").click(function(){
-      $("#panel4").slideToggle("slow");
-    });
-  });
-
-  $(document).ready(function(){
-    $("#flip5").click(function(){
-      $("#panel5").slideToggle("slow");
-    });
-  });
-
-  $(document).ready(function(){
-    $("#flip6").click(function(){
-      $("#panel6").slideToggle("slow");
-    });
-  });
-
-  $(document).ready(function(){
-    $("#flip7").click(function(){
-      $("#panel7").slideToggle("slow");
-    });
-  });
-
-  $(document).ready(function(){
-    $("#flip8").click(function(){
-      $("#panel8").slideToggle("slow");
-    });
-  });
-
 
   function darkMode() {
     var element = document.body;
